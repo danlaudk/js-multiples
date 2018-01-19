4f34a1bedcf169c469ae5af0beb9de0b720bfca4
danlaudk@yahoo.dk <danlaudk@yahoo.dk>
2018-01-19 03:08:17 +0200

    removes scalajs

--- build.sbt
+++ build.sbt
@@ -1,5 +1,4 @@
 // Turn this project into a Scala.js project by importing these settings
-enablePlugins(ScalaJSPlugin)
 
 name := "js-multiples"
 
@@ -7,14 +6,14 @@ version := "0.1-SNAPSHOT"
 
 scalaVersion := "2.12.1"
 
-scalaJSUseMainModuleInitializer := true
-
 //testFrameworks += new TestFramework("utest.runner.Framework")
 
 libraryDependencies ++= Seq(
-    "org.scala-js" %%% "scalajs-dom" % "0.9.1",
-    "com.lihaoyi" %%% "utest" % "0.4.5" % "test",
+//    "org.scala-js" %%% "scalajs-dom" % "0.9.1",
+//    "com.lihaoyi" %%% "utest" % "0.4.5" % "test",
     "org.scalatest" % "scalatest_2.12" % "3.0.4" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.13.4" % Test
+    "org.scalacheck" %% "scalacheck" % "1.13.4" % Test,
+    "org.scalactic" %% "scalactic" % "3.0.4"
+
 
 )
--- src/main/scala/sumOfMultiples/JsSumOfMultiples.scala
+++ src/main/scala/sumOfMultiples/JsSumOfMultiples.scala
@@ -1,34 +1,10 @@
 package sumOfMultiples
 
-import scala.scalajs.js
-import js.annotation.JSExport
-import org.scalajs.dom
 
-@js.native
-trait EventName extends js.Object {
-  type EventType <: dom.Event
-}
+object JsSumOfMultiples {
+  def main(args: Array[String]): Unit = {
+    println("sumofmultiples set( 7 13 17) 20 is " + sumOfMultiples(Set(7, 13, 17), 20))
 
-object EventName {
-  def apply[T <: dom.Event](name: String): EventName { type EventType = T } =
-    name.asInstanceOf[EventName { type EventType = T }]
-
-  val onmousedown = apply[dom.MouseEvent]("onmousedown")
-}
-
-@js.native
-trait ElementExt extends js.Object {
-  def addEventListener(name: EventName)(
-      f: js.Function1[name.EventType, _]): Unit
-}
-
-object JsSumOfMultiples extends js.JSApp {
-  def main(): Unit = {
-    val paragraph = dom.document.createElement("p")
-    paragraph.innerHTML = "<strong> sumOfMUltiples of Set(7, 13, 17), 20  is ...  </strong> " + sumOfMultiples(Set(7, 13, 17), 20)
-    dom.document.getElementById("playground").appendChild(paragraph)
-
-    val p = paragraph.asInstanceOf[ElementExt]
   }
   def sumOfMultiples(s:Set[Int], i:Int):Int = 0
 }
